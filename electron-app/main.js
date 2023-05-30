e9fcb63ea023ab8904e641bb0c7862a6cb5f3acf
Heli Eronen <herojonen@gmail.com>
2023-05-30 14:50:11 +0300

    Managing windows

--- electron-app/main.js
+++ electron-app/main.js
@@ -1,14 +1,28 @@
 const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
 
-  win.loadFile('index.html')
+    win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-  createWindow()
+    createWindow()
+
+    // On MacOS, create a window whenever the app is active
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+// On Windows and Linux, close app when window is closed
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
